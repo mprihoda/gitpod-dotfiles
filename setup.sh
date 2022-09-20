59efcd443515e69c48c1b5b3df2f3741a6270119
Michal Prihoda <michal@prihoda.net>
2022-09-20 10:46:58 +0200

    Fixed setup

--- setup.sh
+++ setup.sh
@@ -2,5 +2,13 @@
 
 set -e
 
-cp $(pwd)/m1/settings.xml ~/.m2/settings.xml
-rm -rf ~/.doom.d && ln -s $(pwd)/doom.d ~/.doom.d && doom sync
+S=$(dirname $0)
+
+cp -r "${S}"/m2 /home/gitpod/.m2
+
+if [ -e /home/gitpod/.doom.d ]
+then
+	        rm -rf /home/gitpod/.doom.d
+fi
+
+ln -s "${S}"/doom.d /home/gitpod/.doom.d && doom sync
