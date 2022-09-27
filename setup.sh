b592ca346f18f1f2895ae40224f678c8dcecbd54
Michal Prihoda <michal@prihoda.net>
2022-09-27 17:02:43 +0200

    Fix the nix path.

--- setup.sh
+++ setup.sh
@@ -2,6 +2,8 @@
 
 set -e
 
+source /home/gitpod/.nix-profile/etc/profile.d/nix.sh
+
 # Clean up the lefover .doom.d from original image
 # The owner has been erroneously root in some version, so sudo
 if [ -e /home/gitpod/.doom.d ]
