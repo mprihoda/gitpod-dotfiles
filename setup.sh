36808fb26b7d56095a61cf4132d5718ee3a33adb
Michal Prihoda <michal@prihoda.net>
2022-10-05 11:08:10 +0200

    Make emacs persistent and run bg daemon.

--- setup.sh
+++ setup.sh
@@ -4,6 +4,12 @@ set -e
 
 source /home/gitpod/.nix-profile/etc/profile.d/nix.sh
 
+# Move doom emacs to workspace, so that it is kept between restarts
+if [ ! -d "/workspace/.emacs.d" ]; then
+	cp -r /home/gitpod/.emacs.d /workspace
+fi
+echo "export EMACSDIR=/workspace/.emacs.d" >> /home/gitpod/.bashrc.d/301-doom-workspace.sh
+
 # Clean up the lefover .doom.d from original image
 # The owner has been erroneously root in some version, so sudo
 if [ -e /home/gitpod/.doom.d ]
@@ -13,3 +19,6 @@ fi
 
 nix build --no-link /home/gitpod/.dotfiles#homeConfigurations.gitpod.activationPackage
 "$(nix path-info /home/gitpod/.dotfiles#homeConfigurations.gitpod.activationPackage)"/activate
+
+# Run the emacs daemon
+emacs --daemon
